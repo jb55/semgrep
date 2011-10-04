9dcedfefbd40c6d1189af2d2ba4cfbd88b4e0780
Bill Casarin <bill@casarin.ca>
2011-10-04 09:09:21 -0400

    Search the generic ast instead of the C ast directly

--- Semgrep.hs
+++ Semgrep.hs
@@ -19,6 +19,12 @@ data BinOp = LeOp
            | UnkOp String
            deriving (Show, Typeable, Data, Eq)
 
+data AssignOp = AssignOp
+              | MulAssignOp
+              | DivAssignOp
+              | UnkAssignOp String
+              deriving (Show, Typeable, Data, Eq)
+
 data Const = IntConst Int
            | CharConst Char
            | FloatConst Float
@@ -27,13 +33,20 @@ data Const = IntConst Int
 
 data Expr = BinaryOp BinOp Expr Expr
           | Var String
+          | Assign AssignOp Expr Expr
           | Const Const
           | ConditionalOp Expr (Maybe Expr) Expr
           | UnkExpr String
           deriving (Show, Typeable, Data)
 
+gtrace a = trace (gshow a) a
 
-gtrace a = trace (gshow a) a 
+fromCAssignOp :: CAssignOp -> AssignOp
+fromCAssignOp CAssignOp = AssignOp
+fromCAssignOp CMulAssOp = MulAssignOp
+fromCAssignOp CDivAssOp = DivAssignOp
+fromCAssignOp o = UnkAssignOp (show o)
+
 
 fromCBinOp :: CBinaryOp -> BinOp
 fromCBinOp CLeOp  = LeOp
@@ -44,20 +57,24 @@ fromCBinOp CEqOp  = EqOp
 fromCBinOp CNeqOp = NeqOp
 fromCBinOp o      = UnkOp (gshow o)
 
+
 fromCConst :: CConst -> Const
-fromCConst (CIntConst cint _) = IntConst (fromInteger $ getCInteger cint)
-fromCConst (CCharConst cchar _) = CharConst (head $ getCChar cchar)
+fromCConst (CIntConst cint _)         = IntConst (fromInteger $ getCInteger cint)
+fromCConst (CCharConst cchar _)       = CharConst (head $ getCChar cchar)
 fromCConst (CFloatConst (CFloat s) _) = FloatConst (read s)
-fromCConst (CStrConst cstring _) = StringConst (getCString cstring)
+fromCConst (CStrConst cstring _)      = StringConst (getCString cstring)
+
 
 fromCExpr :: CExpr -> Expr
-fromCExpr (CVar (Ident s _ _) _) = Var s
-fromCExpr (CConst c)             = Const (fromCConst c)
-fromCExpr (CCond e1 e2 e3 _) = ConditionalOp (fromCExpr e1) 
-                                             (fmap fromCExpr e2) 
-                                             (fromCExpr e3)
-
-fromCExpr (CBinary op e1 e2 _) = BinaryOp (fromCBinOp op) 
+fromCExpr (CVar ident _)       = Var (identToString ident)
+fromCExpr (CAssign op e1 e2 _) = Assign (fromCAssignOp op)
+                                        (fromCExpr e1)
+                                        (fromCExpr e2)
+fromCExpr (CConst c)           = Const (fromCConst c)
+fromCExpr (CCond e1 e2 e3 _)   = ConditionalOp (fromCExpr e1)
+                                               (fmap fromCExpr e2)
+                                               (fromCExpr e3)
+fromCExpr (CBinary op e1 e2 _) = BinaryOp (fromCBinOp op)
                                           (fromCExpr e1)
                                           (fromCExpr e2)
 fromCExpr e = UnkExpr (show e)
@@ -68,62 +85,56 @@ isIfExpr (CIf _ _ _ _) = True
 isIfExpr _ = False
 
 
-isCondOp :: CBinaryOp -> Bool
-isCondOp CLeOp   = True
-isCondOp CGrOp   = True
-isCondOp CLeqOp  = True
-isCondOp CGeqOp  = True
-isCondOp CEqOp   = True
-isCondOp CNeqOp  = True
-isCondOp _       = False
+isCondOp :: BinOp -> Bool
+isCondOp LeOp   = True
+isCondOp GrOp   = True
+isCondOp LeqOp  = True
+isCondOp GeqOp  = True
+isCondOp EqOp   = True
+isCondOp NeqOp  = True
+isCondOp _      = False
 
 
-isCondExpr :: CExpr -> Bool
-isCondExpr (CCond _ _ _ _ )   = True
-isCondExpr (CBinary op _ _ _) = isCondOp op
-isCondExpr _                  = False
+isCondExpr :: Expr -> Bool
+isCondExpr (ConditionalOp op _ _ ) = True
+isCondExpr (BinaryOp op _ _ )      = isCondOp op
+isCondExpr _                       = False
 
-
-isCond :: CStat -> Bool
-isCond (CIf expr _ _ _)  = isCondExpr expr
-isCond (CExpr expr _ )   = maybe False isCondExpr expr
-isCond _                 = False
 
 
 ifStmts :: CTranslUnit -> [CStat]
 ifStmts = listify isIfExpr
 
 
-conditions :: CTranslUnit -> [CExpr]
-conditions = listify isCondExpr
+conditions :: [Expr] -> [Expr]
+conditions = filter isCondExpr
 
 
 process file = do
   putStrLn $ "file: " ++ file
   stream <- readInputStream file
-  let parsedC = parseC stream nopos
+  let parsedC = parseC stream (initPos file)
 
   case parsedC of
     Left txt -> print txt
     Right transUnit -> do
-      let conds = conditions transUnit
-      let all   = allExprs transUnit
-      let ifs   = ifStmts transUnit
+      let all   = allCExprs transUnit
       let convertedExprs = map fromCExpr all
+      let conds = conditions convertedExprs
+      let ifs   = ifStmts transUnit
 
-      putStrLn "\nCondition Expressions"
-      mapM_ prettyPrint conds
+      putStrLn "\nAll Expressions"
+      mapM_ prettyPrint all
+      putStrLn "\nConditional Expressions"
+      mapM_ print conds
       putStrLn "\nIf Statements"
       mapM_ prettyPrint ifs
-      putStrLn "\nConverted Expressions"
-      mapM_ print convertedExprs
 
   where
     prettyPrint = putStrLn . show . pretty
 
-    allExprs :: CTranslUnit -> [CExpr]
-    allExprs = listify (const True)
-
+    allCExprs :: CTranslUnit -> [CExpr]
+    allCExprs = listify (const True)
 
 main = do
   [file] <- getArgs
